15149aae065052ad2b2d23642191130184984e8a
Zhengyang Duan <zyduan522@gmail.com>
2018-07-16 09:13:48 -0400

    done

--- MLImageIdentifier/controller/ViewController.swift
+++ MLImageIdentifier/controller/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
 
@@ -24,9 +26,30 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let targetImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
             image.image = targetImage
+            guard let ciimage = CIImage(image: targetImage) else {fatalError("cant convert image ") }
+            detect(image: ciimage)
         }
         imagePicker.dismiss(animated: true, completion: nil)
     }
+    
+    func detect(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{fatalError("loading failed")}
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {fatalError("request failed")}
+            if let firstResult = results.first {
+                self.navigationItem.title = firstResult.identifier
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do{
+            try handler.perform([request])
+            
+        }catch{print("request failed")}
+        
+        
+    }
+    
     @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
         
         present(imagePicker, animated: true, completion: nil)
